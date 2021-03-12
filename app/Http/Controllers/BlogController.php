cfab42e5b2939030699d30114d0086f9743c6da7
Kadylbekov <nurik.kad.nurik@gmail.com>
2021-03-12 22:09:51 +0600

    create blog create page

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -11,8 +11,19 @@ class BlogController extends Controller
     //
     public function index()
     {
+
         $posts = Post::all();
 
         return view('post.index')->with(['posts' => $posts]);
     }
+        public function store(Request $request)
+        {
+            Post::create([
+                'title' => $request->title,
+                'body' => $request->body,
+           ]);
+           return back();
+
+        }
 }
+
--- routes/web.php
+++ routes/web.php
@@ -47,6 +47,12 @@ Route::get('/post', function () {
 });
 
 Route::get('blog/index',[BlogController::class, 'index']); 
+Route::get('blog/create',function () {
+return view('post.create');
+});
+
+Route::post('blog/create',[BlogController::class, 'store'])-> name('add-post');
+
     
 
 
